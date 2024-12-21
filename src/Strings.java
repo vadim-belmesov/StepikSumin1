fb92e762dfb33aafa2e2c626f7b9b2a55892bac7
VadimB workbook <vadik.belmesov@yandex.ru>
2024-12-21 07:42:36 +0300

    Изучили тип String

---
+++ src/Strings.java
@@ -0,0 +1,9 @@
+public class Strings {
+  public static void main(String[] args) {
+    String Name = "Иван";
+    int age = 20;
+
+    String s = "Привет " + Name + "!\nТебе: " + age + " лет";
+    System.out.println(s);
+  }
+}
