68d2bcaa149d036fee5e604242348fd4720905ba
VadimB workbook <vadik.belmesov@yandex.ru>
2025-01-06 07:20:29 +0300

    Небольшие правки на Асус

--- src/LoopLess.java
+++ src/LoopLess.java
@@ -16,16 +16,41 @@ public class LoopLess {
                 break;
             }
         }
-    }*/
+    } */
     /*public static void main(String[] args) {
         int a = 0;
         do{
             System.out.println("Hello!");
         } while (a > 0);
     }*/
-    public static void main(String[] args) {
-        for (int i = 1; i < 100; i++) {
+    /* public static void main(String[] args) {
+        //все четные числа от 0 до 1000
+        int i = 0;
+        while( i <= 100){
+            if(i % 2 != 0){
+                System.out.println(i);
+            }
+            i++;
+        }
+    } */
+   /* public static void main(String[] args) {
+    int i = 0;
+    do{
+        System.out.println(i);
+    }while(i > 0);
+   } */
+/*     public static void main(String[] args) {
+        for(int i=0; i < 100; i++){
+            System.out.println(i);
+        }
+    } */
+public static void main(String[] args) {
+    //Вывести при помощи FOR все числа от 1000 до 0 которые делятся на 3 без остатка
+    for(int i = 100; i >= 0; i--){
+        if (i%3 == 0) {
             System.out.println(i);
         }
     }
+}
+
 }
