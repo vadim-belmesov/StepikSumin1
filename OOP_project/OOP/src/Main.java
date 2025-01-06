ff6df9febc6eca84a7ea31dccecf2ce5d89d0820
vadim-belmesov <vadik.belmesov@yandex.ru>
2025-01-06 10:45:22 +0300

    123

--- OOP_project/OOP/src/Main.java
+++ OOP_project/OOP/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         //Экземпляр класса Test
-        Test test = new Test();
+        //Test test = new Test();
         //используем параметризованный метод square
         //System.out.println(test.square(10));
 
