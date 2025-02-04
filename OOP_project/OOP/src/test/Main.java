9293bf558d7736d024aa6905aec8f75a63392643
vadim-belmesov <vadik.belmesov@yandex.ru>
2025-02-04 17:16:36 +0300

    Оболочки типов. Примитивные и их ссылочные аналоги

--- OOP_project/OOP/src/test/Main.java
+++ OOP_project/OOP/src/test/Main.java
@@ -3,23 +3,17 @@ package test;
 import Examples.MyMath;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        MyArrayList employess = getEmployees();
-        employess.add("James");
-        employess.remove("Emma");
+    public static void main(String[] args){
+        String a = "1000";
+        String b = "2000";
 
-        for(int i =0; i < employess.getSize(); i++){
-            System.out.println(employess.get(i));
-        }
-    }
+        System.out.println(a + b); //10002000
 
-    private static MyArrayList getEmployees(){
-        MyArrayList employees = new MyArrayList();
-        employees.add("John");
-        employees.add("Olivia");
-        employees.add("Emma");
-        employees.add("Max");
-        employees.add("Nick");
-        return employees;
+        //Используем метод для преобразования
+        int a1 = Integer.parseInt(a);
+        int b1 = Integer.parseInt(b);
+
+        System.out.println(a1 + b1); //3000
+
     }
 }
