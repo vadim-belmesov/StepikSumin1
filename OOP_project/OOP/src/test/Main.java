4f2e4c448b00883ba7c82ee16bb854b12a0a3354
vadim-belmesov <vadik.belmesov@yandex.ru>
2025-02-07 18:35:37 +0300

    Домашняя по Collection ArrayList

--- OOP_project/OOP/src/test/Main.java
+++ OOP_project/OOP/src/test/Main.java
@@ -3,16 +3,30 @@ package test;
 import Examples.MyMath;
 import Examples.Person;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
-        String text = "This is John. He is 27";
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Ivan");
+        names.add("Petr");
 
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
 
-        Person man = new Person(text.substring(8,12), 0);
-        //String age = text.substring(21,22);
+        ArrayList<String> List = new ArrayList<>();
+        for (String name: names){
+            for (int num:numbers){
+                List.add(name + " - " + num);
+            }
+        }
 
-        man.setAge(Integer.parseInt(text.substring(20,22)));
-        System.out.println(man.getName() + " " + man.getAge());
+        for (String item: List){
+            System.out.println(item);
+        }
 
     }
+
 }
