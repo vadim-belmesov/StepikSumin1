9d983e095a2704a6c455007918835f91ca418fc6
vadim-belmesov <vadik.belmesov@yandex.ru>
2025-02-27 10:31:13 +0300

    Наследование 1 Cat, Lion Конструктор с параметрами, сеттеры и геттеры

--- OOP_project/OOP/src/inheritance/CatFamily.java
+++ OOP_project/OOP/src/inheritance/CatFamily.java
@@ -1,14 +1,38 @@
 package inheritance;
 
 public class CatFamily {
-  public int legs;
-  public int eyes;
-  public boolean canEatPerson;
+  protected int legs;
+  protected int eyes;
+  protected boolean canEatPerson;
 
-  //Конструктор без параметров
+  //Конструктор
   public CatFamily(int legs, int eyes, boolean canEatPerson){
     this.legs = legs;
     this.eyes = eyes;
     this.canEatPerson = canEatPerson;
+  }
+
+  public void setLegs(int legs){
+    this.legs = legs;
+  }
+
+  public void setEyes(int eyes){
+    this.eyes = eyes;
+  }
+
+  public void setCanEatPerson(boolean canEatPerson){
+    this.canEatPerson = canEatPerson;
+  }
+
+  public int getEyes() {
+    return eyes;
+  }
+
+  public int getLegs() {
+    return legs;
+  }
+
+  public boolean isCanEatPerson() {
+    return canEatPerson;
   }
 }
