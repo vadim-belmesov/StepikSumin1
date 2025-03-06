ad2ca14ec8275e53ea893a944bb7e67f59008c7f
vadim-belmesov <vadik.belmesov@yandex.ru>
2025-03-06 16:57:31 +0300

    Java Продвинутый уровень Переопределение метода toString()

--- OOP_project/OOP/src/Examples/Person.java
+++ OOP_project/OOP/src/Examples/Person.java
@@ -4,28 +4,31 @@ public class Person {
     private String name;
     private int age;
 
-    //Сеттер для возраста
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+//Геттеры для имени и возраста
+    public String getName(){
+        return this.name;
+    }
+    public int getAge(){
+        return this.age;
+    }
+
+//Сеттер для возраста
     public void setAge(int age){
         if (age >= 0) {
             this.age = age;
         }else System.out.println("Enter correct age");
     }
-
     public void setName(String name){
         this.name = name;
     }
 
-    //Геттеры для имени и возраста
-    public String getName(){
-        return this.name;
-    }
-
-    public int getAge(){
-        return this.age;
-    }
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
+    @Override
+    public String toString() {
+        return "Имя: " + this.name + " Возраст: " + this.age;
     }
 }
--- OOP_project/OOP/src/test/Main.java
+++ OOP_project/OOP/src/test/Main.java
@@ -1,19 +1,10 @@
 package test;
 
-import Examples.Rectangle;
-import box.Box;
-import box.weightBox;
-import shape.Shape;
-import shape.rectangle;
-import shape.triangle;
+import Examples.Person;
 
 public class Main {
     public static void main(String[] args) {
-        rectangle rectangle1 = new rectangle(10,20);
-        triangle triangle1 = new triangle(10,10,30);
-
-        triangle1.showPerimeter();
-        rectangle1.showPerimeter();
-
+        Person person = new Person("Jo", 20);
+        System.out.println(person); //Examples.Person@3feba861
     }
 }
