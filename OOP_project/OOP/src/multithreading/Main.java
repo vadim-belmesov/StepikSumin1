3e253263c1b35349ec9cf9ea4029cc38c45b32f1
vadim-belmesov <vadik.belmesov@yandex.ru>
2025-03-10 19:38:39 +0300

    Многопоточность 1 2 потока параллельно

--- OOP_project/OOP/src/multithreading/Main.java
+++ OOP_project/OOP/src/multithreading/Main.java
@@ -2,6 +2,7 @@ package multithreading;
 
 public class Main {
   public static void main(String[] args) {
+    //первый поток
     Thread thread = new Thread(new Runnable() {
       //воспользуемся анонимным классом
       @Override
@@ -16,5 +17,22 @@ public class Main {
       }
     });
     thread.start(); //здесь будут выводиться 1
+
+
+  //второй поток
+    Thread thread2 = new Thread(new Runnable() {
+      //воспользуемся анонимным классом
+      @Override
+      public void run() {
+        for (int i = 0; i < 10_000_000;i++){
+          System.out.println(i);
+          try {
+            Thread.sleep(1000); //усыпляем поток на время
+          } catch (InterruptedException e) {
+          }
+        }
+      }
+    });
+      thread2.start(); //здесь будут выводиться 1
   }
 }
